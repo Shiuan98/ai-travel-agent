5298e90f5fd7a38e8974f606ac4e196b33a868fe
Shiuan98 <woeishiuan98@gmail.com>
2026-05-12 16:06:45 +0800

    use hash router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import Home from "../pages/Home.vue";
 import Dashboard from "../pages/Dashboard.vue";
@@ -15,7 +15,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 });
 

69006c0e0427190d0422f7e119c82d867243c568
Shiuan98 <woeishiuan98@gmail.com>
2026-05-12 16:08:47 +0800

    fix vue router rewrite

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "../pages/Home.vue";
 import Dashboard from "../pages/Dashboard.vue";
@@ -15,7 +15,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 });
 
